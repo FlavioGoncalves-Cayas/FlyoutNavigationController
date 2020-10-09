c3d29253cf1827495d94c13a444f2631d827ec0d
Martin Luong <m.luong@cayas.de>
2020-10-09 14:34:39 +0200

    Änderungen

--- FlyoutNavigationControllerDemo/FlyoutNavigationControllerDemo/MainViewController.cs
+++ FlyoutNavigationControllerDemo/FlyoutNavigationControllerDemo/MainViewController.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using CoreGraphics;
 using FlyoutNavigation;
 using MonoTouch.Dialog;
 using UIKit;
 
 namespace FlyoutNavigationControllerDemo
 {
+    public class ButtonViewController : UIViewController
+    {
+        private readonly FlyoutNavigationController _navigation;
+        UIButton _button;
+
+        public ButtonViewController(FlyoutNavigationController navigation)
+        {
+            _navigation = navigation;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.Red;
+            EdgesForExtendedLayout = UIRectEdge.None;
+
+            _button = new UIButton();
+            _button.SetTitle("Test", UIControlState.Normal);
+            _button.BackgroundColor = UIColor.Purple;
+            _button.TouchUpInside += Button_TouchUpInside;
+            base.View.AddSubview(_button);
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            _button.Frame = new CGRect(10, 90, View.Frame.Width - 20, 20);
+        }
+
+        private void Button_TouchUpInside(object sender, EventArgs e)
+        {
+            _navigation.SetCurrentViewController(new UINavigationController(new ContentViewController(_navigation, "Hello World 2", "Hello World 2")));
+        }
+    }
+
     public class MainViewController : UIViewController
     {
-		private FlyoutNavigationController _navigation;
+        private FlyoutNavigationController _navigation;
 
         public MainViewController()
         {
@@ -17,57 +55,25 @@ namespace FlyoutNavigationControllerDemo
         {
             base.ViewDidLoad();
 
-			_navigation = new FlyoutNavigationController(UITableViewStyle.Grouped)
-			{
-				// Create the navigation menu
-				NavigationRoot = new RootElement("Navigation")
-				{
-					new Section("Animals")
-					{
-						new MyStringElement("Cat"),
-						new MyStringElement("Dog"),
-						new MyStringElement("Horse"),
-					},
-					new Section("Vegetables")
-					{
-						new MyStringElement("Cucumber"),
-						new MyStringElement("Lettuce"),
-						new MyStringElement("Pepper")
-					},
-				},
-			};
-			// Supply view controllers corresponding to menu items:
-			_navigation.ViewControllers = new[]
-			{
-					new UINavigationController(new ContentViewController(_navigation, "Cat", "Cat")),
-					new UINavigationController(new ContentViewController(_navigation, "Dog", "Dog")),
-					new UINavigationController(new ContentViewController(_navigation, "Horse", "Horse")),
-					new UINavigationController(new ContentViewController(_navigation, "Cucumber", "Cucumber")),
-					new UINavigationController(new ContentViewController(_navigation, "Lettuce", "Lettuce")),
-					new UINavigationController(new ContentViewController(_navigation, "Pepper", "Pepper"))
-			};
+            var navigationController = new UITabBarController();
 
-			// Specify navigation position
-			_navigation.Position = FlyOutNavigationPosition.Left;
-			_navigation.NavigationTableView.BackgroundColor = UIColor.GroupTableViewBackgroundColor;
-			_navigation.View.Frame = UIScreen.MainScreen.Bounds;
+            _navigation = new FlyoutNavigationController(navigationController);
+            _navigation.SetCurrentViewController(new UINavigationController(new ContentViewController(_navigation, "Hello", "Hello World")));
 
-			View.AddSubview(_navigation.View);
-			AddChildViewController(_navigation);
-		}
+            navigationController.AddChildViewController(new UINavigationController(new ButtonViewController(_navigation) { Title = "Tab1" }));
+            navigationController.AddChildViewController(new UIViewController() { Title = "Tab2" });
+            navigationController.AddChildViewController(new UIViewController() { Title = "Tab3" });
+            navigationController.AddChildViewController(new UIViewController() { Title = "Tab4" });
+            navigationController.AddChildViewController(new UIViewController() { Title = "Tab5" });
+            navigationController.AddChildViewController(new UIViewController() { Title = "Tab6" });
 
-        private class MyStringElement : StringElement
-        {
-            public MyStringElement(string title) : base(title)
-            {
-            }
+            // Specify navigation position
+            _navigation.Position = FlyOutNavigationPosition.Right;
+            _navigation.View.BackgroundColor = UIColor.GroupTableViewBackgroundColor;
+            _navigation.View.Frame = UIScreen.MainScreen.Bounds;
 
-            public override UITableViewCell GetCell(UITableView tv)
-            {
-                var cell = base.GetCell(tv);
-				cell.SelectionStyle = UITableViewCellSelectionStyle.Gray;
-				return cell;
-            }
+            View.AddSubview(_navigation.View);
+            AddChildViewController(_navigation);
         }
     }
 }
